79f19663a9d2a3c4bbf9cc6956fbf4c5fb2b6e1b
Anthony <elmiranthony@gmail.com>
2023-11-28 23:12:21 +0100

    adding userCreate

--- social-media-backend/src/controllers/users.js
+++ social-media-backend/src/controllers/users.js
@@ -10,4 +10,41 @@ const getUser = async (req, res) => {
     }
 };
 
-module.exports = { getUser };
+const createUser = async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, picturePath } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email is already registered' });
+        }
+
+        //TODO: FileUpload profile
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            picturePath,
+        });
+
+        await newUser.save();
+
+        // remove hash before sending user back
+        delete newUser.password;
+
+        res.status(201).json(newUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+module.exports = {
+    createUser,
+};
+
+module.exports = { getUser, createUser };
--- social-media-backend/src/routes/users.js
+++ social-media-backend/src/routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { getUser, getUserFriends } = require('../controllers/users');
+const { getUser, createUser } = require('../controllers/users');
 const { verifyToken } = require('../middleware/jwt');
 const router = express.Router();
 
 router.get('/:id', verifyToken, getUser);
-
+router.post('/', createUser);
 module.exports = router;
