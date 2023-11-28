4e63b770d2ef01c3211707d716fb0012df2e2879
Anthony <elmiranthony@gmail.com>
2023-11-28 22:57:10 +0100

    removing frds

--- social-media-backend/src/routes/users.js
+++ social-media-backend/src/routes/users.js
@@ -4,6 +4,5 @@ const { verifyToken } = require('../middleware/jwt');
 const router = express.Router();
 
 router.get('/:id', verifyToken, getUser);
-router.get('/:id/friends', verifyToken, getUserFriends);
 
 module.exports = router;
